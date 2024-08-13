72e5d4e4bc4584ff4aace34f5fe16da460cbca96
jduda27 <joshua.duda@gmail.com>
2024-08-13 16:51:20 -0500

    added Text Node conversion first imp.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,34 @@
 from textnode import TextNode
 from leafnode import LeafNode
 
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            tN = LeafNode(None,text_node.text)
+            print(tN.to_html())
+        case "bold":
+            tN = LeafNode("b",text_node.text)
+            print(tN.to_html())
+        case "italic":
+            tN = LeafNode("i",text_node.text)
+            print(tN.to_html())
+        case "code":
+            tN = LeafNode("code",text_node.text)
+            print(tN.to_html())
+        case "link":
+            print("link")
+            tN = LeafNode("a",text_node.text,{"href":text_node.url})
+            print(tN.to_html())
+        case "image":
+            tN = LeafNode("img",None,{"src":text_node.url,"alt":text_node.text})
+            print(tN.to_html())
+        case _:
+            raise Exception("Type does not exist.")
+
 node = TextNode("test1", "bold", "www.test.com")
+node2 = TextNode("I'm a link", "link", "www.test.com")
 leafnode = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
 print(node)
 print(leafnode.to_html())
-
-def text_node_to_html_node(text_node):
-    match text_node["type"]:
-        case "text":
-            print("text")
-        case "bold":
-            print("bold")
-        case "italic":
-            print("italic")
-        case "code":
-            print("code")
-        case "link":
-            print("link")
-        case "image":
-            print("image")
+text_node_to_html_node(node) #Move to textnode.py text_node becomes self. 
+text_node_to_html_node(node2)
--- src/textnode.py
+++ src/textnode.py
@@ -16,3 +16,19 @@ class TextNode():
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
+
+    def text_node_to_html_node(text_node):
+        match text_node["type"]:
+            case "text":
+                print("text")
+            case "bold":
+                print("bold")
+            case "italic":
+                print("italic")
+            case "code":
+                print("code")
+            case "link":
+                print("link")
+            case "image":
+                print("image")
